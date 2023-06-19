a277cc2ac138857a00ad9ef354aef58e487988f6
Alexander Barnard <alexwb1@live.com>
2023-06-19 18:28:56 +0700

    got it working, reformatting

--- src/controller/todo_controller.rs
+++ src/controller/todo_controller.rs
@@ -1,26 +1,24 @@
-// pub mod todo_controller {
+use actix_web::{get, post, web, HttpResponse, Responder};
 
-    use actix_web::{get, post, web, HttpResponse, Responder};
-    #[get("/")]
-    pub async fn hello() -> impl Responder {
-        HttpResponse::Ok().body("Hello world!")
-    }
+#[get("/")]
+pub async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
 
-    #[post("/echo")]
-    pub async fn echo(req_body: String) -> impl Responder {
-        HttpResponse::Ok().body(req_body)
-    }
+#[post("/echo")]
+pub async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
 
-    async fn manual_hello() -> impl Responder {
-        HttpResponse::Ok().body("Hey there!")
-    }
+pub async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
 
-    pub fn configure(cfg: &mut web::ServiceConfig) {
-        cfg.service(
-            web::scope("/")
-                .service(hello)
-                .service(echo)
-                .route("/hey", web::get().to(manual_hello)),
-        );
-    }
-// }
+pub fn configure(cfg: &mut web::ServiceConfig) {
+    cfg.service(
+        web::scope("/")
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello)),
+    );
+}
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
-use actix_web::{App, HttpServer};
-
 
 mod controller {
     pub mod todo_controller;
 }
-// mod controller::todo_controller;
-use controller::todo_controller::configure;
+
+use controller::todo_controller::*;
+
+use actix_web::{web, App, HttpServer};
+
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new().configure(configure)
+        App::new()
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
