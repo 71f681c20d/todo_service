4d69c59b41f1aee269346dfd0fd144b3cdc61641
Alexander Barnard <alexwb1@live.com>
2023-06-20 08:37:15 +0700

    Refactored controller and endpoint configuration

--- src/controller/todo_controller.rs
+++ src/controller/todo_controller.rs
@@ -1,27 +1,41 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
 #[get("/")]
-pub async fn hello() -> impl Responder {
+async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
 
 #[get("/hello1")]
-pub async fn hello1() -> impl Responder {
+async fn hello1() -> impl Responder {
     HttpResponse::Ok().body("Hello world!1")
 }
 
 #[get("/hello2")]
-pub async fn hello2() -> impl Responder {
+async fn hello2() -> impl Responder {
     HttpResponse::Ok().body("Hello world!2")
 }
 
 #[post("/echo")]
-pub async fn echo(req_body: String) -> impl Responder {
+async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
 }
 
-pub async fn manual_hello() -> impl Responder {
+async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
 
+pub async fn configure_server() -> actix_web::dev::Server {
+  HttpServer::new(|| {
+      App::new()
+          // Declare all your endpoint routes to be served
+          .service(hello)
+          .service(hello1)
+          .service(hello2)
+          .service(echo)
+          .route("/hey", web::get().to(manual_hello))
+  })
+  .bind(("127.0.0.1", 8080))
+  .expect("Failed to bind server")
+  .run()
+}
 
--- src/main.rs
+++ src/main.rs
@@ -3,30 +3,14 @@ mod controller {
     pub mod todo_controller;
 }
 
-use actix_web::{HttpServer, web, App};
 use controller::todo_controller;
-
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let server = configure_server().await;
+    let server = todo_controller::configure_server().await;
     bind_server(server).await
-}
-
-async fn configure_server() -> actix_web::dev::Server {
-    HttpServer::new(|| {
-        App::new()
-            .service(todo_controller::hello)
-            .service(todo_controller::echo)
-            .route("/hey", web::get().to(todo_controller::manual_hello))
-    })
-    .bind(("127.0.0.1", 8080))
-    .expect("Failed to bind server")
-    .run()
 }
 
 async fn bind_server(server: actix_web::dev::Server) -> std::io::Result<()> {
     server.await
 }
-
-
